cfb36f5f0b09fc0b8dcdae1d1a7459008641daa1
Nessing <35237174+Nessing@users.noreply.github.com>
2021-12-15 15:04:48 +0300

    servlet with filter UTF-8
    http://localhost:8080/hello/ → "Hello World"

---
+++ servlet-example/src/main/java/servlets/EncodingFilter.java
@@ -0,0 +1,16 @@
+package servlets;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class EncodingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletResponse.setContentType("text/html");
+        servletResponse.setCharacterEncoding("utf-8");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
